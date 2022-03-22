6feee17e274c43d0a0222cd3a81a8a634771a275
mrp758 <74987993+mrp758@users.noreply.github.com>
2022-03-22 16:07:52 +0200

    Updated

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "./App.css"
 import PetDB from './petDB.json'
-import './SearchBarS.css'
-import './SearchBarP/HomeS.css'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from '../pages/Home';
 import Form from '../pages/Form';
